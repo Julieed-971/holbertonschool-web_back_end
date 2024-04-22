87ce666b5c013e638bb5fe77bcf0c6866db78bba
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-22 11:05:21 +0200

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Type-annotated concat function taking strings
+as arguments and returning a concatenated string
+"""
+import typing
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings"""
+    return (str1 + str2)
