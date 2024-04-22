71924996a2ea4df19d3e1dfb8334f79f90dc3315
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-22 16:30:54 +0200

    Fixed the max_delay

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -11,6 +11,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay between 0 and max_delay and return it"""
-    actual_delay = random.uniform(0, max_delay + 1)
+    actual_delay = random.uniform(0, max_delay)
     await asyncio.sleep(actual_delay)
     return actual_delay
