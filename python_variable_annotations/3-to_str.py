ddd7b41e384a4d89d98cec4850a1110f630c9ce5
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-22 11:24:02 +0200

    Added new line and fixed pycodestyle

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function to_str taking a float n
+as argument and returning the float string representation
+"""
+
+import typing
+
+
+def to_str(n: float) -> str:
+    """Return float as string"""
+    return str(n)
