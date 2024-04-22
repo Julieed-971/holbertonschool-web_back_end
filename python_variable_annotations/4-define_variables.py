31101201ed5dd39dba72b371965e0916cfd766bb
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-22 11:24:22 +0200

    Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Type-annotate variables with specified values
+"""
+
+import typing
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
