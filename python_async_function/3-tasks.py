e3148f352ec8f5f9257ab5b6bf6bc5c2c1a046ed
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-25 10:58:56 +0200

    Tasks

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Return an asyncio task
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay):
+    """Return an asyncio task"""
+    task = asyncio.Task(wait_random(max_delay))
+    return task
