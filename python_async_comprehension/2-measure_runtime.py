d0dd0dc66382e2621079b6d63b8fd996e66cc018
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-26 00:07:34 +0200

    Run time for four parallel comprehensions

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,10 +12,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Coroutine that measure runtime for executing
-    async_comprehension fout times in parallel
+    async_comprehension four times in parallel
     """
     start_time = time.time()
-    coroutines = [async_comprehension() for _ in range(1, 4)]
+    coroutines = [async_comprehension() for _ in range(4)]
     await asyncio.gather(*coroutines)
     end_time = time.time()
     total_time = end_time - start_time
