ecb6569ee89fe3944c94101348b2caf6bb8aba60
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-25 23:10:47 +0200

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ Coroutine that yield a random number between 0 and 10
 
 import asyncio
 import random
-from typing import List, AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, float]:
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that yield a random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
