7c8d9398e100e483ed5c858889d3479186cf8650
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-30 09:31:18 +0200

    Removed empty list requirement

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -6,9 +6,5 @@ Lists all documents in a collection
 def list_all(mongo_collection):
     """Lists all documents in a collection"""
 
-    # check if the collection is empty
-    if mongo_collection.count_documents({}) == 0:
-        print([])
-
     # if not empty, list and return all documents in collection
     return mongo_collection.find()
