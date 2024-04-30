96d74473c80f3627c7b778c538765daa8d7b77bf
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-30 09:30:08 +0200

    Removed unneccessary database connection

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,13 +3,8 @@
 Lists all documents in a collection
 """
 
-from pymongo import MongoClient
-
-
 def list_all(mongo_collection):
     """Lists all documents in a collection"""
-    # connect to MongoDB
-    MongoClient(host="localhost", port=27017)
 
     # check if the collection is empty
     if mongo_collection.count_documents({}) == 0:
