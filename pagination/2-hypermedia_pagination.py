edaa9cf1e40e3401cb32d6cd85bb5bd07f35598c
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-01 00:10:19 +0200

    Fixed annotation

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -6,7 +6,7 @@ to certain pagination parameters
 
 import csv
 import math
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -57,7 +57,7 @@ class Server:
         except AssertionError:
             Exception
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """
         Returns a dictionnary containing various
         information about dataset pagination
