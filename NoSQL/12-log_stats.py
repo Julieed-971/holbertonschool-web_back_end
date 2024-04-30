ffc0024a245ca5c6e6676d807188e1e2e460d733
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-30 15:08:44 +0200

    Added guard

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,28 +3,28 @@
 
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
+    # Counts the total number of documents in the nginx_collection
+    logs_count = nginx_collection.count_documents({})
+    print("{} logs".format(logs_count))
 
-# Counts the total number of documents in the nginx_collection
-logs_count = nginx_collection.count_documents({})
-print("{} logs".format(logs_count))
+    # Print the count of ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    # methods in documents in logs
+    print("Methods:")
+    print("\tmethod GET: {}".format(nginx_collection.count_documents(
+        {"method": "GET"})))
+    print("\tmethod POST: {}".format(nginx_collection.count_documents(
+        {"method": "POST"})))
+    print("\tmethod PUT: {}".format(nginx_collection.count_documents(
+        {"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(nginx_collection.count_documents(
+        {"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(nginx_collection.count_documents(
+        {"method": "DELETE"})))
 
-# Print the count of ["GET", "POST", "PUT", "PATCH", "DELETE"]
-# methods in documents in logs
-print("Methods:")
-print("\tmethod GET: {}".format(nginx_collection.count_documents(
-    {"method": "GET"})))
-print("\tmethod POST: {}".format(nginx_collection.count_documents(
-    {"method": "POST"})))
-print("\tmethod PUT: {}".format(nginx_collection.count_documents(
-    {"method": "PUT"})))
-print("\tmethod PATCH: {}".format(nginx_collection.count_documents(
-    {"method": "PATCH"})))
-print("\tmethod DELETE: {}".format(nginx_collection.count_documents(
-    {"method": "DELETE"})))
-
-# Print the number of documents with method=GET and path=/status
-print("{} status check".format(nginx_collection.count_documents(
-    {"method": "GET", "path": "/status"})))
+    # Print the number of documents with method=GET and path=/status
+    print("{} status check".format(nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})))
