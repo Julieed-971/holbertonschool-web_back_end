a79b2afeffafb5d9925b1cd0d1857661f4eab10b
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-30 11:45:26 +0200

    Added function docstring

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,6 +3,7 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name"""
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
