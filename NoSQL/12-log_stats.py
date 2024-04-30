851dc73a8b9f0c672a620bdc1b8750df7f587ff1
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-30 14:57:19 +0200

    Added function docstring

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 
 def nginx_stats():
-
+    """Provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
