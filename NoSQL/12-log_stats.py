bc26c6862813533865668ccfd133eac4136ed2e8
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-30 15:00:39 +0200

    Removed docstring

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,7 +4,6 @@
 from pymongo import MongoClient
 
 
-"""Provides some stats about Nginx logs stored in MongoDB"""
 client = MongoClient('mongodb://127.0.0.1:27017')
 nginx_collection = client.logs.nginx
 
