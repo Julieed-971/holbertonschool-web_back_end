e41bd5c36b67af2e7ab9a7589c4bff4d9f7df00d
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-08 18:25:30 +0200

    Changed return statement in displayFullPrice

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -32,7 +32,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
