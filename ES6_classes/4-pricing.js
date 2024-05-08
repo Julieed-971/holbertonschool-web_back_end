0c6fd30f7eacc945360f50c4a6a60cd8cf9081f7
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-08 18:09:14 +0200

    Pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+/* eslint-disable-next-line import/extensions */
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = new Currency(currency.code, currency._name);
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = value;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class');
+    } else {
+      this._currency = value;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    } else {
+      return amount * conversionRate;
+    }
+  }
+}
