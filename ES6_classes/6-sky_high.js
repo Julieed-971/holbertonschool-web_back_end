02186b668e6a2d0513f52f33123238097364cc69
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-08 21:28:19 +0200

    Inheritance

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+/* eslint-disable-next-line import/extensions */
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super();
+    this._sqft = sqft;
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
