7b1c3b8dc107eeba2ee4c1fb11ec8b47998f7651
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-16 15:26:38 +0200

    Moved log response

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -4,8 +4,8 @@ const isSuccessful = {
 };
 
 export default function handleResponseFromAPI(promise) {
+  console.log('Got a response from the API');
   return promise
     .then(isSuccessful)
-    .catch(() => new Error())
-    .then(console.log('Got a response from the API'));
+    .catch(() => new Error());
 }
