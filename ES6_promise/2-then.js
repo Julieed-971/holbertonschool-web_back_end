36a2fab570730e6378b4d91033e92764816dc6a4
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-16 15:39:26 +0200

    Optimized code

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,5 @@
-const isSuccessful = {
-  status: '200',
-  body: 'success',
-};
-
 export default function handleResponseFromAPI(promise) {
-  promise.then(isSuccessful);
+  promise.then({ status: '200', body: 'success' });
   promise.catch(() => new Error());
   promise.then(console.log('Got a response from the API'));
 }
