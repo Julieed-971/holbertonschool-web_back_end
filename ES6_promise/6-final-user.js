41a53a7e1bbb9fa217faeec8a55a367a9232570d
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-17 23:56:40 +0200

    Trying yet another method

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,5 +3,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
+  return Promise.any([uploadPhoto(filename), signUpUser(firstName, lastName)]);
 }
