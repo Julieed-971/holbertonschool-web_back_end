21ec4c254e45bdc6d2033df30b2b0985644a145e
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-17 23:05:24 +0200

    Is it a joke?

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,16 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)])
-    .then((results) => results.map((result) => {
-      const promiseStatus = {
-        status: result.status,
-      };
-      if (result.status === 'fulfilled') {
-        promiseStatus.value = result.value;
-      } else {
-        promiseStatus.value = result.reason;
-      }
-      return promiseStatus;
-    }));
+  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
 }
