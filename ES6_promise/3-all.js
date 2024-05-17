b56fecfc7464df8603a8a635c811ebafb049afff
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-17 16:54:24 +0200

    Removed unnecessary new array

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,9 +5,9 @@ export default function handleProfileSignup() {
   // Collectively resolve all promises
   return Promise.all([uploadPhoto(), createUser()])
     // Use then callback to access promises properties values
-    .then(([photoData, userData]) => {
+    .then(([uploadPhoto, createUser]) => {
       // Log values to the console
-      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+      console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
     })
     // In case of error, log error message
     .catch(() => new Error('Signup system offline'));
