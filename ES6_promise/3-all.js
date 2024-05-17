3f2c296101114f50426806adf8567e3b004d224b
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-17 16:28:59 +0200

    Trying with async function

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,19 @@
 /* eslint-disable-next-line import/extensions */
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  // Collectively resolve all promises
-  return Promise.all([uploadPhoto(), createUser()])
-    // Use then callback to access promises properties values
-    .then(([photoData, userData]) => {
-      // Log values to the console
-      console.log(photoData.body, userData.firstName, userData.lastName);
-    })
-    // In case of error, log error message
-    .catch(() => new Error('Signup system offline'));
+export default async function handleProfileSignup() {
+  try {
+    const [photoData, userData] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(photoData.body, userData.firstName, userData.lastName);
+  } catch (error) {
+    throw new Error('Signup system offline');
+  }
 }
+  // // Collectively resolve all promises
+  // return Promise.all([uploadPhoto(), createUser()])
+  //   // Use then callback to access promises properties values
+  //   .then(([photoData, userData]) => {
+  //     // Log values to the console
+  //   })
+  //   // In case of error, log error message
+  //   .catch(() => new Error('Signup system offline'));
