d1193524e8c59dc47e027544c772269e27ae6405
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-17 16:15:33 +0200

    Removed destructuring

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,14 +2,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const utils = [uploadPhoto(), createUser()];
   // Collectively resolve all promises
-  return Promise.all(utils)
-    // Log message to the console
-    .then(([photoData, userData]) => {
-      const { body } = photoData;
-      const { firstName, lastName } = userData;
-      console.log(body, firstName, lastName);
+  return Promise.all([uploadPhoto(), createUser()])
+    // Use then callback to access promises properties values
+    .then((values) => {
+      const photoData = values[0];
+      const userData = values[1];
+      // Log values to the console
+      console.log(photoData.body, userData.firstName, userData.lastName);
     })
     // In case of error, log error message
     .catch(() => new Error('Signup system offline'));
