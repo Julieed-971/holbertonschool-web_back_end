44a498daf18a497d157968a5c1f6b29084afa1c2
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-17 23:32:30 +0200

    My output is correct locally

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,7 @@
+/* eslint-disable-next-line import/extensions */
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)])
+  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
 }
