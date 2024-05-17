0554c5db44b138ba15303474c744c092df659cbb
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-17 23:29:23 +0200

    My output is correct locally

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,15 +3,4 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)])
-    .then((results) => results.map((result) => {
-      const promiseStatus = {
-        status: result.status,
-      };
-      if (result.status === 'fulfilled') {
-        promiseStatus.value = result.value;
-      } else {
-        promiseStatus.value = result.reason;
-      }
-      return promiseStatus;
-    }));
 }
