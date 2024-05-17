532a88448a7174c677e9733f396c759625cf1f2f
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-17 20:06:06 +0200

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,19 +3,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return new Promise((resolve, reject) => {
-    Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)])
-      .then((results) => {
-        const promiseStatuses = results.map((result) => {
-          if (result.status === 'fulfilled') {
-            return { status: 'fulfilled', value: result.value };
-          }
-          return { status: 'rejected', value: result.reason };
-        });
-        resolve(promiseStatuses);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)])
+    .then((results) => results.map((result) => {
+      const promiseStatus = {
+        status: result.status,
+      };
+      if (result.status === 'fulfilled') {
+        promiseStatus.value = result.value;
+      } else {
+        promiseStatus.value = result.reason;
+      }
+
+      return promiseStatus;
+    }));
 }
