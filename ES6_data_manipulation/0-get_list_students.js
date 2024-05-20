3c50ad5e904d2ab612d51422a44781563f059450
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-20 15:45:19 +0200

    I'll be damned if this doesn't pass the checker

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,7 @@
 export default function getListStudents() {
   return [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Fransisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Fransisco',
-    },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
