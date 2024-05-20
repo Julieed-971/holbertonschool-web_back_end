5d5c04829259360b9b4d88182a450e4d9559d494
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-20 15:43:30 +0200

    Bloody typo on Colombia

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,19 @@
 export default function getListStudents() {
   return [
-    { id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
-    { id: 2, firstName: 'James', location: 'Colombia' },
-    { id: 5, firstName: 'Serena', location: 'San Fransisco' },
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Fransisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Fransisco',
+    },
   ];
 }
