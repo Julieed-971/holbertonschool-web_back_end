cdea28551fc3da95495ef7635a05384f85b2ea88
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-20 15:21:42 +0200

    Fixed lint error

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
-export default getListStudents => {
-	return [
-		{
-			id: 1,
-			firstName: 'Guillaume',
-			location: 'San Fransisco'
-		},
-		{
-			id: 2,
-			firstName: 'James',
-			location: 'Colombia'
-		},
-		{
-			id: 5,
-			firstName: 'Serena',
-			location: 'San Fransisco'
-		},
-	];
+export default function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Fransisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Colombia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Fransisco',
+    },
+  ];
 }
