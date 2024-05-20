14a5462289b2a6045dbc1318023c63588f743008
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-20 15:39:01 +0200

    Go check yourself!

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,7 @@
 export default function getListStudents() {
-  const listStudents = [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Fransisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Colombia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Fransisco',
-    },
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
+    { id: 2, firstName: 'James', location: 'Colombia' },
+    { id: 5, firstName: 'Serena', location: 'San Fransisco' },
   ];
-  return listStudents;
 }
