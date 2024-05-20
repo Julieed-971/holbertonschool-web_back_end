8fdce94bdd856ed940db79b97034d096ce4a086a
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-20 15:15:53 +0200

    Basic list of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default getListStudents => {
+	return [
+		{
+			id: 1,
+			firstName: 'Guillaume',
+			location: 'San Fransisco'
+		},
+		{
+			id: 2,
+			firstName: 'James',
+			location: 'Colombia'
+		},
+		{
+			id: 5,
+			firstName: 'Serena',
+			location: 'San Fransisco'
+		},
+	];
+}
