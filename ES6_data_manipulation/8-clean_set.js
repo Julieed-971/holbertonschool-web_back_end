82dc64a6ae142f13d12150df69e2407d175e3d6a
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-22 19:16:59 +0200

    Revert to previous code as it didn't changed a thing

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,17 @@
 export default function cleanSet(set, startString) {
+  // Return an empty string if startString is empty
   if (!startString) {
     return '';
   }
+  // Convert the Set object to an array
+  const arrayFromSet = [...set];
 
-  const result = [];
-  for (const element of set) {
-    if (element.startsWith(startString)) {
-      result.push(element.slice(startString.length));
-    }
-  }
-  return result.join('-');
+  // Filter the array to only include elements that start with startString
+  const filteredArray = arrayFromSet.filter((element) => element.startsWith(startString));
+
+  // Remove the startString from the beginning of each element in the array
+  const trimmedArray = filteredArray.map((element) => element.slice(startString.length));
+
+  // Join the elements of the array into a string, separated by '-'
+  return trimmedArray.join('-');
 }
