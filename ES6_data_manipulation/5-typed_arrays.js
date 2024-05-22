6995faebc9d2b87c417285a45d4237ae804d98d8
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-22 14:21:16 +0200

    Added error handling

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,18 @@
 export default function createInt8TypedArray(length, position, value) {
-  // Return a new array buffer with an Int8 value at a specific position
+  // Create an ArrayBuffer of the given length
   const buffer = new ArrayBuffer(length);
+
+  // Create a DataView for the buffer
   const view = new DataView(buffer);
+
+  // If the position is out of range, throw an error
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  // Set the value at the given position in the DataView as an 8-bit signed integer
   view.setInt8(position, value);
+
+  // Return the DataView
   return view;
 }
