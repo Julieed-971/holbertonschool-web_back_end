2209724458e63c1f7757c5ff8fdbcb613975253d
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-22 18:20:12 +0200

    Map data structure for groceries list

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const groceriesMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return groceriesMap;
+}
