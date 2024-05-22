63ab35fa8e405ff6e7225b53e245a2acefca879e
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-22 19:15:17 +0200

    Changed code with a for of loop

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,13 @@
 export default function cleanSet(set, startString) {
-  // Return an empty string if startString is empty
-  if (startString === '') {
+  if (!startString) {
     return '';
   }
-  // Convert the Set object to an array
-  const arrayFromSet = [...set];
 
-  // Filter the array to only include elements that start with startString
-  const filteredArray = arrayFromSet.filter((element) => element.startsWith(startString));
-
-  // Remove the startString from the beginning of each element in the array
-  const trimmedArray = filteredArray.map((element) => element.slice(startString.length));
-
-  // Join the elements of the array into a string, separated by '-'
-  return trimmedArray.join('-');
+  const result = [];
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      result.push(element.slice(startString.length));
+    }
+  }
+  return result.join('-');
 }
