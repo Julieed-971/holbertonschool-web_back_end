72b6bca931e5f382c1e9d010986cc57282b9ed8f
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-04-25 00:48:58 +0200

    Changed doc

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
+"""BasicCache module"""
 
 from base_caching import BaseCaching
 
-"""BasicCache module
-"""
-
 
 class BasicCache(BaseCaching):
-    """
-    BasicCache is a caching system that inherits from BaseCaching. It provides
-    basic functionality to store and retrieve items in a cache.
-    """
+    """BasicCache is a caching system that inherits from BaseCaching.
+    It provides basic functionality to store and retrieve items in a cache."""
 
     def put(self, key, item):
-        """
-        Store an item in the cache.
-        """
+        """Store an item in the cache."""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Retrieve an item from the cache.
-        """
+        """Retrieve an item from the cache."""
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
