d0ab91d717a549525efa429e4b9b258da9272387
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-04-25 10:22:01 +0200

    MRU Caching

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""BasicCache module"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """FIFOCache is a caching system that inherits from BaseCaching.
+    It implements a First-In-First-Out (FIFO) caching mechanism."""
+
+    def __init__(self):
+        """Initialize the cache and an order list to track insertion order."""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Store an item in the cache and remove the first
+        one if number of elements in dict exceed the MAX"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+            self.cache_data[key] = item
+            self.order.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                mru_key = self.order.pop(-2)
+                del self.cache_data[mru_key]
+                print(f"DISCARD: {mru_key}")
+
+    def get(self, key):
+        """Retrieve an item from the cache."""
+        if key is not None and key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
