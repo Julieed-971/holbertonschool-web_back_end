3d98b7a77970060e60bc6a16babb065278309b42
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-06 16:14:48 +0200

    Changed PII fields

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 import logging
 import re
 from typing import List
-PII_FIELDS = ('email', 'phone', 'password', 'ssn', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
 
 
 def filter_datum(fields: List[str], redaction: str,
