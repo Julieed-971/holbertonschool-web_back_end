38b0b895c99b7738abf132a2e2292d3a2e6d4d97
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-06 10:21:23 +0200

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import re
+from typing import List
 
 """Module that returns the log message obfuscated"""
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Function that returns the log message obfuscated"""
     for field in fields:
-        pattern = rf"({field}=)(.*?){separator}"
-        message = re.sub(pattern, rf"\1{redaction}{separator}", message)
+        message = re.sub((field + "=.*?" + separator),
+                         (field + "=" + redaction + separator), message)
     return message
