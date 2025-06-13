49f097facfb79d24b138300b8a94e5da88eedecc
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-13 14:16:56 +0200

    Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized_route() -> str:
+    """GET /api/v1/unauthorized
+    Returns:
+    str: _description_"""
+    abort(401)
