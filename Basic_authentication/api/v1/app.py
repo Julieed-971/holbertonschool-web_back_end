fe97770d78b7542e4b5de31c0b780b9491e0b467
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-14 19:49:16 +0200

    Request validation!

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,6 +12,25 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                  '/api/v1/forbidden/']
+
+if getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def check_auth():
+    """Handle authentication request"""
+    if auth is None:
+        return
+    if auth.do_not_require_auth(request.path, excluded_paths) is True:
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,19 +8,24 @@ from models.base import Base
 class Auth(Base):
     """Auth class"""
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def do_not_require_auth(self, path: str,
+                            excluded_paths: List[str]) -> bool:
         """Define which routes don't need authentication"""
         if path is None or excluded_paths is None or not excluded_paths:
             return True
         if not path.endswith('/'):
             path += "/"
+        excluded_paths = [p if p.endswith('/') else p + '/'
+                          for p in excluded_paths]
         if path not in excluded_paths:
             return True
         return False
 
     def authorization_header(self, request=None) -> str:
         """Returns None"""
-        return None
+        if request is None or request.headers.get("Authorization") is None:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None"""
