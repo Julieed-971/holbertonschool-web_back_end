42ff3fc6eeec91c758274d5e8304d07de1ae31b7
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-14 20:07:06 +0200

    Basic auth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -16,7 +16,10 @@ auth = None
 excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                   '/api/v1/forbidden/']
 
-if getenv("AUTH_TYPE") == "auth":
+if getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for basic authentication"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic auth class that inherit from Auth"""
