166b0ae9bd4b2148d43699efdbeeb4f0fabd6607
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-14 21:13:06 +0200

    Basic - Base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module for basic authentication"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                               str) -> str:
+        """returns the decoded value of
+        a Base64 string base64_authorization_header"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_string = base64.b64decode(base64_authorization_header)
+            return decoded_string.decode("utf-8")
+        except Exception as e:
+            return None
