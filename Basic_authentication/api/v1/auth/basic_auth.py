2268abc7b9e3cf35c078a0ca49db961ef2a774cf
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-14 20:42:48 +0200

    Basic - Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic auth class that inherit from Auth"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """returns the Base64 part of the
+        Authorization header for a Basic Authentication"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
