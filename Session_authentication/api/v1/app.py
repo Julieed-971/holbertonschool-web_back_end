b8ec02928dc9bc862ece4c24d4ed20f9f89edc2e
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-24 11:01:33 +0200

    Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -13,8 +13,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+excluded_paths = [
+    '/api/v1/status/',
+    '/api/v1/unauthorized/',
+    '/api/v1/forbidden/',
+    '/api/v1/auth_session/login/'
+]
 
 if getenv("AUTH_TYPE") == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
@@ -33,13 +37,11 @@ def check_auth():
     if auth is None:
         return
     if auth.do_not_require_auth(request.path, excluded_paths) is True:
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
         if auth.authorization_header(
                 request) is None and auth.session_cookie(request) is None:
             abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
         request.current_user = auth.current_user(request)
 
 
