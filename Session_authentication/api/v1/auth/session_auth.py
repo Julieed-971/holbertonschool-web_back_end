cb0acbfda4e3efbc225d469b140da925fe4b372f
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-24 10:06:39 +0200

    User ID for Session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,9 @@ class SessionAuth(Auth):
         self.session_id = str(uuid.uuid4())
         self.user_id_by_session_id[self.session_id] = user_id
         return self.session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID"""
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
