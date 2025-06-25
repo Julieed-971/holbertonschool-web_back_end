6cf8099ebbe37b381aac889680e772f5fd465380
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-25 10:06:14 +0200

    Added annotation

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Save a user to the database
            Return: User object"""
         new_user = User(email=email, hashed_password=hashed_password)
