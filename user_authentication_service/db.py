572e2981174dbc5dcf831c3336dba675dd6049e2
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-25 10:34:36 +0200

    Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -3,6 +3,8 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
@@ -38,3 +40,13 @@ class DB:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs: str) -> User:
+        """Takes in arbitrary keyword arguments and
+        returns the first row found in the users table
+        as filtered by the method’s input arguments"""
+        session = self._session
+        found_user = session.query(User).filter_by(**kwargs).first()
+        if found_user is None:
+            raise NoResultFound
+        return found_user
