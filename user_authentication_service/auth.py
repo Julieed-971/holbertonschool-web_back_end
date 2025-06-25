4c3e57684df0f86705ea112068432c373b16b35d
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-25 12:17:49 +0200

    Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """Module to authenticate users"""
+from db import DB
+from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -10,3 +13,22 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     return hash
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user if doesn't exist in the database"""
+        db = self._db
+        try:
+            db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+        return new_user
