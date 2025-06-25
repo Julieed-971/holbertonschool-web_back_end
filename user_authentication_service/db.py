52c863880461b613f3eef958084b0c6ce3cbabed
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-25 10:35:42 +0200

    Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
         session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs: str) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """Takes in arbitrary keyword arguments and
         returns the first row found in the users table
         as filtered by the method’s input arguments"""
