78c5ba4d888947e17ecc643ce437f34b1b944af8
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-26 10:01:54 +0200

    Get reset password token

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -57,5 +57,16 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Reset a password token"""
+    try:
+        email = request.form.get('email')
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
