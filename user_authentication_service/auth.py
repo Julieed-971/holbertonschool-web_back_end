e994bd374fc6a1ccb09f27f516e5236b8f29a720
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-06-26 10:09:12 +0200

    Update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -89,3 +89,15 @@ class Auth:
             return new_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a user's password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(
+                user.id,
+                hashed_password=hashed_password,
+                reset_token=None)
+        except NoResultFound:
+            raise ValueError
