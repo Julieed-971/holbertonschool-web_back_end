9e340867676aa814e8e4e6b21dd0a732bbb00c76
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-07-11 16:29:14 +0200

    Parameterize

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -53,3 +53,13 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock_get_json.assert_called_once()
             mock_public_repos_url.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test has_license return value"""
+        self.assertEqual(GithubOrgClient.has_license(
+            repo,
+            license_key), expected)
