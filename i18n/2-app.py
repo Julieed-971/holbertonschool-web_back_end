a2cbcb7bc8ed765b991e243ac847880e5994c05c
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-07-18 12:11:53 +0200

    Get locale from request

---
+++ i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Module for a basic Flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Configuration for available languages in app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    """Basic function that return a basic template"""
+    return render_template("0-index.html")
+
+
+def get_locale():
+    """Get the best match locale language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+if __name__ == "__main__":
+    app.run()
