6b608b637ee3624676f9d534b6259eeba5a798b5
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-07-18 11:51:53 +0200

    Basic Flask app

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """Module for a basic Flask app"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     """Basic function that return a basic template"""
-    return "<title>Welcome to Holberton</title><h1>Hello world</h1>"
+    return render_template("./templates/0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
