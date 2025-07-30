a2aabaa07308dad5ef5b81aa21a0498877c104c9
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-07-30 14:56:42 +0200

    Removed pipes

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str | bytes | int | float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis with a random key."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
