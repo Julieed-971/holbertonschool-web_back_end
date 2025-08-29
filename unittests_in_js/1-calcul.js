f6a60eda9d0c03ac6b861045b7cbf71e456d5c9d
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-08-29 15:50:53 +0200

    Combining descriptions

---
+++ unittests_in_js/1-calcul.js
@@ -0,0 +1,18 @@
+const calculateNumber = (type, a, b) => {
+    a = Math.round(a);
+    b = Math.round(b);
+    if (type === 'SUM') {
+        return a + b;
+    }
+    else if (type === 'SUBSTRACT') {
+        return a - b
+    }
+    else if (type === 'DIVIDE') {
+        if (b === 0)  {
+            return 'Error'
+        }
+        return a / b
+    }
+}
+
+module.exports = calculateNumber;
---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,36 @@
+const assert = require("assert")
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+    it('should return 4 when adding 1 and 3', () => {
+        assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    });
+
+    it('should return 6 when adding 1.4 and 4.5', () => {
+        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+
+    it('should return -4 when substracting 1.4 and 4.5', () => {
+        assert.strictEqual(calculateNumber('SUBSTRACT', 1.4, 4.5), -4);
+    });
+
+    it('should return 0.2 when dividing 1.4 and 4.5', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return \'Error\' when dividing 1.4 and 0', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    })
+
+    it('should round 0.4 down to 0', () => {
+        assert.strictEqual(calculateNumber('SUM', 0.4, 0), 0);
+    });
+
+    it('should round 0.5 up to 1', () => {
+        assert.strictEqual(calculateNumber('SUM', 0.5, 0), 1);
+    });
+
+    it('should handle negative numbers', () => {
+        assert.strictEqual(calculateNumber('SUM', -1.4, -1.6), -3);
+    });
+});
