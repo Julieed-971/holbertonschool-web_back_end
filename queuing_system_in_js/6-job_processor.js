3300fed79728b68d2c33be9391cbd0d5dd0eddf2
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-09-21 13:52:24 +0200

    Create the Job processor

---
+++ queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,12 @@
+import kue from 'kue';
+const queue = kue.createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+queue.process('push_notification_code', (job, done) => {
+    const {phoneNumber, message} = job.data;
+    sendNotification(phoneNumber, message);
+    done();
+});
