74002ffbf945fd35f7716dd254c8af2511a58f3e
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-09-21 13:52:08 +0200

    Changed jobData to match task example

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -2,18 +2,19 @@ import kue from 'kue';
 const queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: "+330795740747",
-  message: "Hello, hello, hello",
+  phoneNumber: "4153518780",
+  message: "This is the code to verify your account",
 }
 
-const job = queue.create('push_notification_code', jobData);
-
-job.on('enqueue', () => {
-    console.log(`Notification job created: ${job.id}`)
+const job = queue.create('push_notification_code', jobData)
+.on('enqueue', () => {
+console.log(`Notification job created: ${job.id}`);
+})
+.on('complete', () => {
+console.log("Notification job completed");
+})
+.on('failed', () => {
+console.log("Notification job failed")
+}).save(function (err) {
+    if (!err) console.log(job.id);
 });
-job.on('complete', () => {
-    console.log("Notification job completed");
-})
-job.on('failed', () => {
-    console.log("Notification job failed")
-})
