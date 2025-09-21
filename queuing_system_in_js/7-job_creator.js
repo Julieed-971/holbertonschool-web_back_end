adaa5f6b110066ab92a59b92ee368694fa6cdb62
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-09-21 14:55:19 +0200

    Track progress and errors with Kue: Create the Job creator

---
+++ queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,67 @@
+import kue from 'kue';
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account'
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account'
+  }
+];
+
+const queue = kue.createQueue();
+
+jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_2', jobData)
+    .on('enqueue', () => {
+        console.log(`Notification job created: ${job.id}`);
+    })
+    .on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+    })
+    .on('failed', (errorMessage) => {
+        console.log(`Notification job ${job.id} failed: ${errorMessage}`);
+    })
+    .on('progress', (progress, data) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+    })
+    .save();
+});
