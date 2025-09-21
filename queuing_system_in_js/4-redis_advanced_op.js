804167868adc7781c22b408748fb5d4a276a0b46
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-09-21 12:31:13 +0200

    Revert to original script

--- queuing_system_in_js/4-redis_advanced_op.js
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -13,30 +13,18 @@ client.on('error', (err) => {
     console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
-client.hset('HolbertonSchools', 'Portland', 50, (err, reply) => {
-    redis.print(err, reply);
-});
-client.hset('HolbertonSchools', 'Seattle', 80, (err, reply) => {
-    redis.print(err, reply);
-});
-client.hset('HolbertonSchools', 'New York', 20, (err, reply) => {
-    redis.print(err, reply);
-});
-client.hset('HolbertonSchools', 'Bogota', 20, (err, reply) => {
-    redis.print(err, reply);
-});
-client.hset('HolbertonSchools', 'Cali', 40, (err, reply) => {
-    redis.print(err, reply);
-});
-client.hset('HolbertonSchools', 'Paris', 2, (err, reply) => {
-    redis.print(err, reply);
-    client.hgetall('HolbertonSchools', (err, obj) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(obj);
-        client.quit();
-    });
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
 
+client.hgetall('HolbertonSchools', (err, obj) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(obj);
+    client.quit();
 });
